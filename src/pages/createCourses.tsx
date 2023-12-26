effc9d35eb97994808feb213335324e3d2099e27
singhjatin297 <singhjatin297@gmail.com>
2023-12-27 03:16:48 +0530

    added cards for courses

---
+++ src/pages/createCourses.tsx
@@ -0,0 +1,220 @@
+import React from 'react'
+import { type Course } from "@prisma/client";
+import { api } from '@/utils/api'
+import { useQuery } from '@tanstack/react-query'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import {
+  Dialog,
+  DialogContent,
+  DialogClose,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { useForm, SubmitHandler } from "react-hook-form"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useRouter } from "next/router";
+import { Textarea } from "@/components/ui/textarea"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import courses from './courses';
+
+const formSchema = z.object({
+  title: z.string().min(2, {
+    message: "Title must be at least 2 characters.",
+  }),
+  description: z.string().min(2, {
+    message: "Description must be at least 2 characters.",
+  }),
+  price: z.string().min(2, {
+    message: "Price must be at least 2 characters.",
+  }),
+})
+
+function Cards({course}: {course: Course}) {
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>{course.title}</CardTitle>
+        <CardDescription>{course.description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+      {course.description}
+      {course.price}
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+  )
+}
+
+
+export default function creatorCourses ()  {
+
+  const createPost = api.post.createCourse.useMutation();
+  const listings = api.get.getAll.useQuery();
+  
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      price: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    createPost.mutateAsync({
+      ...values,
+    })
+
+    console.log(values)
+    form.reset();
+  }
+ 
+
+  return (
+    <div>
+      <div className='flex flex-col left-0 fixed p-3 w-1/4'>
+      <div className='px-2 py-1 text-3xl font-bold'>
+          <Link href="/">
+           COURSERA
+          </Link>
+      </div>
+      <div className='py-10'>
+      <Sheet>
+  <SheetTrigger asChild>
+    <Button className='w-1/4'> ☰</Button>  
+    </SheetTrigger>
+  <SheetContent side={'left'}>
+    <SheetHeader>
+      <SheetTitle className='text-2xl px-6'>Main Menu</SheetTitle>
+      <SheetDescription asChild className='flex flex-col'>
+      <div className='px-6'>
+        <br/>
+        <Link href={"/courses"}>
+        Courses
+        </Link>
+        <br/>
+        <Link href={"/purchases"}>
+        My Courses
+        </Link>
+        <br/>
+        <Link href={"/createCourses"}>
+        Created Courses
+        </Link>
+      </div>
+      </SheetDescription>
+    </SheetHeader>
+  </SheetContent>
+</Sheet>
+</div>
+</div>
+
+<div className='flex justify-center scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl py-2 text-slate-300'>
+        CREATED COURSES
+</div>
+<div className='flex justify-center text-slate-300 py-8'>
+ <Dialog>
+  <DialogTrigger asChild>
+    <Button className="border-slate-500 border-2" variant="outline">ADD COURSE</Button>
+  </DialogTrigger>
+  <DialogContent className="sm:max-w[425px]">
+  <DialogHeader>
+      <DialogTitle className='flex justify-center'>Add new Course</DialogTitle>
+  </DialogHeader> 
+    <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Name your title" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Textarea
+                         id="description"
+                         placeholder="Add description"
+                         className="col-span-3"
+                         {...field}
+                       />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="price"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Price</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Price" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="submit">Submit</Button>
+          </DialogClose>  
+        </DialogFooter>
+        </form>
+      </Form>
+  </DialogContent>
+ </Dialog>
+</div>
+<div>
+{listings?.data?.map((course) => (
+    <Cards key={course.id} course={course} />
+  ))}
+</div>
+    </div>
+  )
+}
---
+++ src/server/api/trpc.ts
@@ -0,0 +1,121 @@
+/**
+ * YOU PROBABLY DON'T NEED TO EDIT THIS FILE, UNLESS:
+ * 1. You want to modify request context (see Part 1)
+ * 2. You want to create a new middleware or type of procedure (see Part 3)
+ *
+ * tl;dr - this is where all the tRPC server stuff is created and plugged in.
+ * The pieces you will need to use are documented accordingly near the end
+ */
+
+/**
+ * 1. CONTEXT
+ *
+ * This section defines the "contexts" that are available in the backend API
+ *
+ * These allow you to access things like the database, the session, etc, when
+ * processing a request
+ *
+ */
+import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
+import * as trpc from '@trpc/server';
+import * as trpcNext from '@trpc/server/adapters/next';
+import { getAuth } from "@clerk/nextjs/server";
+import type {
+  SignedInAuthObject,
+  SignedOutAuthObject,
+  AuthObject
+} from "@clerk/nextjs/server";
+
+import { prisma } from "../db";
+
+interface AuthContext {
+  auth: AuthObjectWithDeprecatedResources<SignedInAuthObject | SignedOutAuthObject>;
+}
+
+type AuthObjectWithDeprecatedResources<T extends AuthObject> = Omit<T, "user" | "organization" | "session"> & {
+  user: T["user"];
+  organization: T["organization"];
+  session: T["session"];
+}
+
+/**
+ * This helper generates the "internals" for a tRPC context. If you need to use
+ * it, you can export it from here
+ *
+ * Examples of things you may need it for:
+ * - testing, so we dont have to mock Next.js' req/res
+ * - trpc's `createSSGHelpers` where we don't have req/res
+ * @see https://create.t3.gg/en/usage/trpc#-servertrpccontextts
+ */
+export const createContextInner = async ({ auth }: AuthContext  ) => {
+  return {
+    auth,
+    prisma,
+  }
+}
+
+/**
+ * This is the actual context you'll use in your router. It will be used to
+ * process every request that goes through your tRPC endpoint
+ * @link https://trpc.io/docs/context
+ */
+export const createTRPCContext = async (
+  opts: trpcNext.CreateNextContextOptions
+) => {
+  return await createContextInner({ auth: getAuth(opts.req) })
+}
+ 
+export type Context = trpc.inferAsyncReturnType<typeof createTRPCContext>;
+
+/**
+ * 2. INITIALIZATION
+ *
+ * This is where the trpc api is initialized, connecting the context and
+ * transformer
+ */
+import { initTRPC, TRPCError } from "@trpc/server";
+import superjson from "superjson";
+
+const t = initTRPC.context<typeof createTRPCContext>().create({
+  transformer: superjson,
+  errorFormatter({ shape }) {
+    return shape;
+  },
+});
+
+// check if the user is signed in, otherwise through a UNAUTHORIZED CODE
+const isAuthed = t.middleware(({ next, ctx }) => {
+  if (!ctx.auth.userId) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return next({
+    ctx: {
+      auth: ctx.auth,
+    },
+  });
+});
+/**
+ * 3. ROUTER & PROCEDURE (THE IMPORTANT BIT)
+ *
+ * These are the pieces you use to build your tRPC API. You should import these
+ * a lot in the /src/server/api/routers folder
+ */
+
+/**
+ * This is how you create new routers and subrouters in your tRPC API
+ * @see https://trpc.io/docs/router
+ */
+export const createTRPCRouter = t.router;
+
+/**
+ * Public (unauthed) procedure
+ *
+ * This is the base piece you use to build new queries and mutations on your
+ * tRPC API. It does not guarantee that a user querying is authorized, but you
+ * can still access user session data if they are logged in
+ */
+export const publicProcedure = t.procedure;
+
+// export this procedure to be used anywhere in your application
+export const protectedProcedure = t.procedure.use(isAuthed);
